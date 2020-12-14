8127d042e00aadf73337b870a3b2b517ba30c306
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-12-14 22:06:26 +0900

    update

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import pandas as pd
 # 変換したいJSONファイルを読み込む
 df = pd.read_json("./json/main.json")
 
+# print(df)
+
 # CSVに変換して任意のファイル名で保存
-df.to_csv("./csv/main.csv")
+df.to_csv("./csv/main.csv", index=False)
